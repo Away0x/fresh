6f7ecafbfe17b8257320ed177d11dcbf05fc01a8
Away0x <wutong0910@foxmail.com>
2019-06-27 10:47:31 +0800

    log

--- runner/runner.go
+++ runner/runner.go
@@ -7,6 +7,7 @@ import (
 
 func run() bool {
 	runnerLog("Running...")
+	runnerLog(buildPath() + " " + buildCommands() + " " + buildFlags())
 
 	cmd := exec.Command(buildPath(), buildCommands(), buildFlags())
 
