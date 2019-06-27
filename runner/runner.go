4aa76cccf8df759e47c04e4428c54b1b524f1d2d
Away0x <wutong0910@foxmail.com>
2019-06-27 10:35:43 +0800

    add command and flags config

--- runner/runner.go
+++ runner/runner.go
@@ -8,7 +8,7 @@ import (
 func run() bool {
 	runnerLog("Running...")
 
-	cmd := exec.Command(buildPath())
+	cmd := exec.Command(buildPath(), buildCommands(), buildFlags())
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
--- runner/settings.go
+++ runner/settings.go
@@ -33,6 +33,9 @@ var settings = map[string]string{
 	"log_color_runner":  "green",
 	"log_color_watcher": "magenta",
 	"log_color_app":     "",
+
+	"run_command": "",
+	"run_flags":   "",
 }
 
 var colors = map[string]string{
@@ -125,6 +128,14 @@ func buildPath() string {
 		p += ".exe"
 	}
 	return p
+}
+
+func buildCommands() string {
+	return settings["run_command"]
+}
+
+func buildFlags() string {
+	return settings["run_flags"]
 }
 
 func buildErrorsFileName() string {
